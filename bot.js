76aa2d3adeb56141ab73ed1993ae0d0f46209dfc
ExtraHash <extrahash@protonmail.com>
2019-02-11 00:32:40 -0600

    add error handling for discord api

--- bot.js
+++ bot.js
@@ -39,14 +39,12 @@ async function update() {
     Globals.satPrice = Math.round(Globals.ogreBTCInfo.price * 100000000);
     Globals.avgTx = Globals.networkInfo.tx_count / Globals.networkInfo.height;
     Globals.netHash = Globals.networkInfo.hashrate / 1000000
-	
 	if (Globals.geckoInfo.price_change_percentage_24h > 0) {
 		Globals.gainsEmoji = `📈`;
 	}
 	else {
 		Globals.gainsEmoji = `📉`;
 	}
-	
 }
 
 async function init() {
@@ -68,7 +66,6 @@ logger.add(new logger.transports.Console, {
 logger.level = 'debug';
 
 // Initialize Discord Bot
-
 (async () => {
     await init();
 })()
@@ -84,6 +81,8 @@ bot.on('ready', (evt) => {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
+bot.on('error', console.error);
+
 bot.on('message', (user, userID, channelID, message, evt) => {
 
     // It will listen for messages that will start with `!`
@@ -93,15 +92,15 @@ bot.on('message', (user, userID, channelID, message, evt) => {
         if (cmd === 'price') {
             bot.sendMessage({
                 to: channelID,
-                message: `🐢 **TurtleCoin Market Info** 🐢\n\n` +
-                         `Rank: **${Globals.geckoInfo.market_cap_rank}**\n\n` +
-                         `Price LTC: **${Globals.litPrice.toFixed(0)} litoshi**\n` +
-                         `Price BTC: **${Globals.satPrice.toFixed(0)} satoshi**\n` +
-                         `Price USD Per Million: **$${Globals.pricePerMillion.toFixed(2)}**\n\n` +
-                         `24h Change: **${Globals.geckoInfo.price_change_percentage_24h.toFixed(2)}%** ${Globals.gainsEmoji}\n` +
-                         `24h Volume: **$${numberWithCommas(Globals.geckoInfo.total_volume.toFixed(2))}**\n` +
-                         `Market Cap: **$${numberWithCommas(Globals.geckoInfo.market_cap.toFixed(2))}**\n` +
-                         `Current Supply: **${numberWithCommas(Globals.geckoInfo.circulating_supply)} TRTL**`
+                message:    `🐢 **TurtleCoin Market Info** 🐢\n\n` +
+                            `Rank: **${Globals.geckoInfo.market_cap_rank}**\n\n` +
+                            `Price LTC: **${Globals.litPrice.toFixed(0)} litoshi**\n` +
+                            `Price BTC: **${Globals.satPrice.toFixed(0)} satoshi**\n` +
+                            `Price USD Per Million: **$${Globals.pricePerMillion.toFixed(2)}**\n\n` +
+                            `24h Change: **${Globals.geckoInfo.price_change_percentage_24h.toFixed(2)}%** ${Globals.gainsEmoji}\n` +
+                            `24h Volume: **$${numberWithCommas(Globals.geckoInfo.total_volume.toFixed(2))}**\n` +
+                            `Market Cap: **$${numberWithCommas(Globals.geckoInfo.market_cap.toFixed(2))}**\n` +
+                            `Current Supply: **${numberWithCommas(Globals.geckoInfo.circulating_supply)} TRTL**`
             });
 
 		if (cmd === 'price') {
@@ -113,34 +112,33 @@ bot.on('message', (user, userID, channelID, message, evt) => {
 		if (cmd === 'network') {
             bot.sendMessage({
                 to: channelID,
-                message: `🐢 **TurtleCoin Network Info** 🐢\n\n` +
-                         `Network Hashrate: **${Globals.netHash.toFixed(2)} MH/s**\n` +
-                         `Current Height: **${numberWithCommas(Globals.networkInfo.height)}**\n\n` +
-                         `Avg TX/Block: **${Globals.avgTx.toFixed(2)}**\n` +
-						 `TX in Mempool: **${numberWithCommas(Globals.networkInfo.tx_pool_size)}**\n` +
-                         `Total Nodes: **${numberWithCommas(Globals.totalNodes)}**`
+                message:    `🐢 **TurtleCoin Network Info** 🐢\n\n` +
+                            `Network Hashrate: **${Globals.netHash.toFixed(2)} MH/s**\n` +
+                            `Current Height: **${numberWithCommas(Globals.networkInfo.height)}**\n\n` +
+                            `Avg TX/Block: **${Globals.avgTx.toFixed(2)}**\n` +
+						    `TX in Mempool: **${numberWithCommas(Globals.networkInfo.tx_pool_size)}**\n` +
+                            `Total Nodes: **${numberWithCommas(Globals.totalNodes)}**`
             });
         }
 
 		if (cmd === 'market') {
             bot.sendMessage({
                 to: channelID,
-                message: `💸 **Top 10 Crypto Markets By Market Capitalization:** 💸\n\n` +
-                         `**     Symbol           Price                       Change 24h**\n` +
-                         `1.  ${Globals.top10[0].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[0].current_price.toFixed(2))}            ${Globals.top10[0].price_change_percentage_24h.toFixed(2)}%\n` +
-                         `2.  ${Globals.top10[1].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[1].current_price.toFixed(2))}            ${Globals.top10[1].price_change_percentage_24h.toFixed(2)}%\n` +
-                         `3.  ${Globals.top10[2].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[2].current_price.toFixed(2))}            ${Globals.top10[2].price_change_percentage_24h.toFixed(2)}%\n` +
-                         `4.  ${Globals.top10[3].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[3].current_price.toFixed(2))}            ${Globals.top10[3].price_change_percentage_24h.toFixed(2)}%\n` +
-                         `5.  ${Globals.top10[4].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[4].current_price.toFixed(2))}            ${Globals.top10[4].price_change_percentage_24h.toFixed(2)}%\n` +
-                         `6.  ${Globals.top10[5].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[5].current_price.toFixed(2))}            ${Globals.top10[5].price_change_percentage_24h.toFixed(2)}%\n` +
-                         `7.  ${Globals.top10[6].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[6].current_price.toFixed(2))}            ${Globals.top10[6].price_change_percentage_24h.toFixed(2)}%\n` +
-                         `8.  ${Globals.top10[7].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[7].current_price.toFixed(2))}            ${Globals.top10[7].price_change_percentage_24h.toFixed(2)}%\n` +
-                         `9.  ${Globals.top10[8].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[8].current_price.toFixed(2))}            ${Globals.top10[8].price_change_percentage_24h.toFixed(2)}%\n` +
-                         `10.  ${Globals.top10[9].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[9].current_price.toFixed(2))}            ${Globals.top10[9].price_change_percentage_24h.toFixed(2)}%`
+                message:    `💸 **Top 10 Crypto Markets By Market Capitalization:** 💸\n\n` +
+                            `**     Symbol           Price                       Change 24h**\n` +
+                            `1.  ${Globals.top10[0].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[0].current_price.toFixed(2))}            ${Globals.top10[0].price_change_percentage_24h.toFixed(2)}%\n` +
+                            `2.  ${Globals.top10[1].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[1].current_price.toFixed(2))}            ${Globals.top10[1].price_change_percentage_24h.toFixed(2)}%\n` +
+                            `3.  ${Globals.top10[2].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[2].current_price.toFixed(2))}            ${Globals.top10[2].price_change_percentage_24h.toFixed(2)}%\n` +
+                            `4.  ${Globals.top10[3].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[3].current_price.toFixed(2))}            ${Globals.top10[3].price_change_percentage_24h.toFixed(2)}%\n` +
+                            `5.  ${Globals.top10[4].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[4].current_price.toFixed(2))}            ${Globals.top10[4].price_change_percentage_24h.toFixed(2)}%\n` +
+                            `6.  ${Globals.top10[5].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[5].current_price.toFixed(2))}            ${Globals.top10[5].price_change_percentage_24h.toFixed(2)}%\n` +
+                            `7.  ${Globals.top10[6].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[6].current_price.toFixed(2))}            ${Globals.top10[6].price_change_percentage_24h.toFixed(2)}%\n` +
+                            `8.  ${Globals.top10[7].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[7].current_price.toFixed(2))}            ${Globals.top10[7].price_change_percentage_24h.toFixed(2)}%\n` +
+                            `9.  ${Globals.top10[8].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[8].current_price.toFixed(2))}            ${Globals.top10[8].price_change_percentage_24h.toFixed(2)}%\n` +
+                            `10.  ${Globals.top10[9].symbol.toUpperCase()}                 $${numberWithCommas(Globals.top10[9].current_price.toFixed(2))}            ${Globals.top10[9].price_change_percentage_24h.toFixed(2)}%`
 
             });
         }
-
 
 		if (cmd === 'help') {
             bot.sendMessage({
