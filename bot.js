6bb70814ce190a35815afddde294f74b9f761b62
ExtraHash <ExtraHash@protonmail.com>
2019-04-16 22:41:26 -0600

    trying to move the data inside of a class

--- bot.js
+++ bot.js
@@ -4,7 +4,27 @@ const request = require('request-promise');
 const fs = require('fs');
 let brainlets = require('./brainlets.json');
 const config = require('./config.json')
-const marketID = '413877823489703947';
+
+// data class
+class Data {
+    async classinit() {
+        await this.dataupdate();
+        setInterval(this.dataupdate, 10000);
+    }
+    async dataupdate() {
+        this.geckoInfo = (await getData('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=turtlecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false', 'geckoTRTLInfo'))[0];
+        this.geckoLTCPrice = (await getData('https://api.coingecko.com/api/v3/coins/markets?vs_currency=ltc&ids=turtlecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=7d'))[0];
+        this.geckoBTCPrice = (await getData('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=turtlecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=7d', 'geckoBTCPrice'))[0];
+        this.networkInfo = await getData('https://blockapi.turtlepay.io/block/header/top', 'networkQuery');
+        this.transactionInfo = await getData('https://blockapi.turtlepay.io/transaction/pool', 'transactionQuery');
+        this.litecoinInfo = (await getData('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=litecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false', 'geckoLTCInfo'))[0];
+        this.bitcoinInfo = (await getData('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false', 'geckoBTCInfo'))[0];
+        this.totalNodes = await getData('https://shellmap.mine2gether.com/api/stats', 'shellmaps');
+        console.log(this.networkInfo);
+    }
+};
+
+const data = new Data();
 
 // variable area
 const Globals = {
@@ -22,6 +42,8 @@ const bot = new discord.Client({
     token: config.authtoken,
     autorun: true
 });
+
+
 
 // function to add a reaction
 function botReact(channelID, evt, reaction) {
@@ -51,8 +73,7 @@ init();
 
 // refreshes variables every 5s
 async function init() {
-    await update();
-    setInterval(update, 5000);
+    await data.classinit();
 }
 
 // get data from http request and store it in variable
@@ -85,6 +106,7 @@ async function update() {
     Globals.litecoinInfo = (await getData('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=litecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false', 'geckoLTCInfo'))[0];
     Globals.bitcoinInfo = (await getData('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false', 'geckoBTCInfo'))[0];
     Globals.totalNodes = await getData('https://shellmap.mine2gether.com/api/stats', 'shellmaps');
+    console.log(data);
 }
 
 // reconnect if disconected
@@ -95,15 +117,6 @@ bot.on('disconnect', function() {
 
 // error logging
 bot.on('error', console.error);
-
-// on new member joining
-bot.on('guildMemberAdd', (member) => {
-    console.log('** New member joined server, welcome message sent');
-    bot.sendMessage({
-        to: marketID,
-        message: `Hey <@${member.id}>, welcome to TurtleCoin!`
-    });
-});
 
 // on message handling
 bot.on('message', (user, userID, channelID, message, evt) => {
@@ -159,51 +172,6 @@ bot.on('message', (user, userID, channelID, message, evt) => {
             }
         };
 
-        // unbrainlet command
-        if (cmd === 'unbrainlet') {
-            if (config.adminID.indexOf(userID) > -1) {
-                if (args[0] === '<' && args[1] === '@') {
-                    let formerBrainlet;
-                    let brainletID = (args.slice(0, -1)).slice(2);
-                    if (brainletID[0] != '!') {
-                        formerBrainlet = brainletID;
-                    } else {
-                        formerBrainlet = brainletID.slice(1);
-                    }
-                    let brainletArray = brainlets;
-                    if (brainletArray.indexOf(formerBrainlet) === -1) {
-                        console.log('** requested brainlet removal that was not in the array');
-                        botReact(channelID, evt, '🚫');
-                    } else {
-                        console.log('** brainlet removed')
-                        botReact(channelID, evt, '☑');
-                        for (var i = brainletArray.length - 1; i >= 0; i--) {
-                            if (brainletArray[i] === formerBrainlet) {
-                                brainletArray.splice(i, 1);
-                            }
-                        }
-                        fs.writeFile('./brainlets.json', JSON.stringify(brainletArray), function(err) {
-                            if (err) throw err;
-                        });
-                        brainlets = brainletArray;
-                    }
-                };
-            } else {
-                botReact(channelID, evt, {name: 'brainlet', id: '556550665095086080'});
-                let brainletArray = brainlets;
-                if (brainletArray.indexOf(userID) > -1) {
-                    console.log('** unauthorized user that is already in the array');
-                } else {
-                    brainletArray.push(userID);
-                    fs.writeFile('./brainlets.json', JSON.stringify(brainletArray), function(err) {
-                        if (err) throw err;
-                        console.log('** automatic new brainlet stored')
-                    });
-                    brainlets = brainletArray;
-                }
-            }
-        };
-
         if (cmd === 'clearbrainlets') {
             if (config.adminID.indexOf(userID) > -1) {
                 botReact(channelID, evt, '☑');
@@ -232,7 +200,7 @@ bot.on('message', (user, userID, channelID, message, evt) => {
         // difficulty command
         if (cmd === 'difficulty') {
             // check that none of the variables are undefined
-            if (Globals.networkInfo === undefined) {
+            if (data.networkInfo.difficulty === undefined) {
                 console.log('** Undefined difficulty requested');
                 botReact(channelID, evt, '🚫');
                 bot.sendMessage({
@@ -244,7 +212,7 @@ bot.on('message', (user, userID, channelID, message, evt) => {
                 botReact(channelID, evt, '☑');
                 bot.sendMessage({
                     to: channelID,
-                    message: `The current difficulty is **${numberWithCommas(Globals.networkInfo.difficulty)}**`
+                    message: `The current difficulty is **${numberWithCommas(data.networkInfo.difficulty)}**`
                 });
             }
         }
@@ -399,7 +367,7 @@ bot.on('message', (user, userID, channelID, message, evt) => {
                 });
             } else {
                 console.log('** Price info message sent');
-                botReact(channelID, evt, '☑');
+                botReact(channelID, evt, getGainsEmoji());
                 bot.sendMessage({
                     to: channelID,
                     embed: {
@@ -452,6 +420,51 @@ bot.on('message', (user, userID, channelID, message, evt) => {
                 });
             }
         }
+
+        // unbrainlet command
+        if (cmd === 'unbrainlet') {
+            if (config.adminID.indexOf(userID) > -1) {
+                if (args[0] === '<' && args[1] === '@') {
+                    let formerBrainlet;
+                    let brainletID = (args.slice(0, -1)).slice(2);
+                    if (brainletID[0] != '!') {
+                        formerBrainlet = brainletID;
+                    } else {
+                        formerBrainlet = brainletID.slice(1);
+                    }
+                    let brainletArray = brainlets;
+                    if (brainletArray.indexOf(formerBrainlet) === -1) {
+                        console.log('** requested brainlet removal that was not in the array');
+                        botReact(channelID, evt, '🚫');
+                    } else {
+                        console.log('** brainlet removed')
+                        botReact(channelID, evt, '☑');
+                        for (var i = brainletArray.length - 1; i >= 0; i--) {
+                            if (brainletArray[i] === formerBrainlet) {
+                                brainletArray.splice(i, 1);
+                            }
+                        }
+                        fs.writeFile('./brainlets.json', JSON.stringify(brainletArray), function(err) {
+                            if (err) throw err;
+                        });
+                        brainlets = brainletArray;
+                    }
+                };
+            } else {
+                botReact(channelID, evt, {name: 'brainlet', id: '556550665095086080'});
+                let brainletArray = brainlets;
+                if (brainletArray.indexOf(userID) > -1) {
+                    console.log('** unauthorized user that is already in the array');
+                } else {
+                    brainletArray.push(userID);
+                    fs.writeFile('./brainlets.json', JSON.stringify(brainletArray), function(err) {
+                        if (err) throw err;
+                        console.log('** automatic new brainlet stored')
+                    });
+                    brainlets = brainletArray;
+                }
+            }
+        };
 
         // whine command
         if (cmd === 'whine') {
